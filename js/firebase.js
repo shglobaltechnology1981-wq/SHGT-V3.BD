5fbb836c2182e98c383864d6d99cdbb8c544c9d0
SHGT-V2.0 <shglobaltechnology1981@gmail.com>
2026-07-29 11:21:12 +0600

    Add Firebase configuration and initialization

---
+++ js/firebase.js
@@ -0,0 +1,44 @@
+//==================================================
+// SH GLOBAL TECHNOLOGY
+// FIREBASE CONFIG
+//==================================================
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+import {
+    getStorage
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-storage.js";
+
+//==================================================
+// FIREBASE CONFIG
+//==================================================
+
+const firebaseConfig = {
+
+    apiKey: "YOUR_API_KEY",
+
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+
+    projectId: "YOUR_PROJECT_ID",
+
+    storageBucket: "YOUR_PROJECT.appspot.com",
+
+    messagingSenderId: "YOUR_SENDER_ID",
+
+    appId: "YOUR_APP_ID"
+
+};
+
+//==================================================
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export const storage = getStorage(app);
+
+console.log("✅ Firebase Connected Successfully");
