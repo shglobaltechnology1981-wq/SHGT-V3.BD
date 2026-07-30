e8bf210cbe7a77d3d88051c13bf87c74fb0b5d88
SHGT-V2.0 <shglobaltechnology1981@gmail.com>
2026-07-30 09:08:55 +0600

    Update cloudinary.js

--- js/cloudinary.js
+++ js/cloudinary.js
@@ -3,46 +3,28 @@
 // Cloudinary Configuration
 //==================================================
 
-
 export const cloudName = "ywxg2gao";
+export const uploadPreset = "Shgobalbd";
 
-export const uploadPreset = "Shgobalbd";";
-// Upload Function
-
-export async function uploadToCloudinary(file){
-
+export async function uploadToCloudinary(file) {
 
     const formData = new FormData();
-
-
-    formData.append(
-        "file",
-        file
-    );
-
-
-    formData.append(
-        "upload_preset",
-        uploadPreset
-    );
-
+    formData.append("file", file);
+    formData.append("upload_preset", uploadPreset);
 
     const response = await fetch(
-
         `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
-
         {
-            method:"POST",
-            body:formData
+            method: "POST",
+            body: formData
         }
-
     );
-
 
     const data = await response.json();
 
+    if (!response.ok) {
+        throw new Error(data.error?.message || "Cloudinary upload failed");
+    }
 
     return data.secure_url;
-
-
 }
