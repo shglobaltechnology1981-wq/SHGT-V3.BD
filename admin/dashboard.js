a76d7874f027d5287dc5e33ba048c30df4ac4f24
SHGT-V2.0 <shglobaltechnology1981@gmail.com>
2026-08-03 16:48:49 +0600

    Update dashboard.js

--- admin/dashboard.js
+++ admin/dashboard.js
@@ -2066,25 +2066,6 @@ loadDashboard();
 
 
 
-//==================================================
-// FIRESTORE DELETE IMPORT
-//==================================================
-
-
-import {
-
-deleteDoc,
-
-doc
-
-}
-
-from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
-
-
-
-
-
 
 
 //==================================================
